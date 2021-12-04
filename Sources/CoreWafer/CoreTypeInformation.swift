929bce093c1a379543e9e146eaff85fae30313a6
Shakshi3104 <pikanchisato@yahoo.co.jp>
2021-12-04 14:15:26 +0900

    Implemented CoreTypeInformation

---
+++ Sources/CoreWafer/CoreTypeInformation.swift
@@ -0,0 +1,80 @@
+//
+//  CoreTypeInformation.swift
+//  
+//
+//  Created by MacBook Pro on 2021/12/04.
+//
+import DeviceHardware
+
+#if os(macOS)
+typealias XDeviceHardware = MacDeviceHardware
+#elseif os(iOS)
+typealias XDeviceHardware = UIDeviceHardware
+#endif
+
+// MARK: - Obtain core type information
+public struct CoreTypeInformation {
+    private let deviceHardware: XDeviceHardware
+    public let coreTypes: [WaferCoreTypes]
+    
+    public init() {
+        #if os(macOS)
+        deviceHardware = MacDeviceHardware.deviceHardware
+        #elseif os(iOS)
+        deviceHardware = UIDeviceHardware.deviceHardware
+        #endif
+        
+        let processorName = deviceHardware.processorName
+        
+        print("\(deviceHardware.processorCount)-core \(processorName)")
+        
+        if let chip = AppleSilicon(rawValue: processorName) {
+            let efficiencyCores: Int
+            let performanceCores: Int
+            
+            switch chip {
+            case .A11:
+                efficiencyCores = 4
+                performanceCores = 2
+            case .A12:
+                efficiencyCores = 4
+                performanceCores = 2
+            case .A12X, .A12Z:
+                efficiencyCores = 4
+                performanceCores = 4
+            case .A13:
+                efficiencyCores = 4
+                performanceCores = 2
+            case .A14:
+                efficiencyCores = 4
+                performanceCores = 2
+            case .A15:
+                efficiencyCores = 4
+                performanceCores = 2
+            case .M1:
+                efficiencyCores = 4
+                performanceCores = 4
+            case .M1Pro:
+                efficiencyCores = 2
+                performanceCores = deviceHardware.processorCount - efficiencyCores
+            case .M1Max:
+                efficiencyCores = 2
+                performanceCores = 8
+            default:
+                // Not big.LITTLE
+                efficiencyCores = 0
+                performanceCores = 0
+            }
+            
+            if efficiencyCores == 0 && performanceCores == 0 {
+                // Not big.LITTLE
+                coreTypes = Array(repeating: WaferCoreTypes.plain, count: deviceHardware.processorCount)
+            } else {
+                // big.LITTLE
+                coreTypes = Array(repeating: WaferCoreTypes.efficiency, count: efficiencyCores) + Array(repeating: WaferCoreTypes.performance, count: performanceCores)
+            }
+        } else {
+            coreTypes = Array(repeating: WaferCoreTypes.plain, count: deviceHardware.processorCount)
+        }
+    }
+}
---
+++ Sources/CoreWafer/WaferCoreTypes.swift
@@ -0,0 +1,45 @@
+//
+//  File.swift
+//  
+//
+//  Created by MacBook Pro on 2021/12/04.
+//
+
+// MARK: - Core type
+public enum WaferCoreTypes: String {
+    /// efficiency core
+    case efficiency = "Efficiency"
+    /// performance core
+    case performance = "Performance"
+    /// not efficency / performance core (all same type core)
+    case plain = ""
+}
+
+// MARK: - Apple Silicon for iPhone, iPad, iPod touch and Mac
+enum AppleSilicon: String {
+    // A Series
+    case A4 = "Apple A4"
+    case A5 = "Apple A5"
+    case A5X = "Apple A5X"
+    case A6 = "Apple A6"
+    case A6X = "Apple A6X"
+    case A7 = "Apple A7"
+    case A8 = "Apple A8"
+    case A8X = "Apple A8X"
+    case A9 = "Apple A9"
+    case A9X = "Apple A9X"
+    case A10 = "Apple A10 Fusion"
+    case A10X = "Apple A10X Fusion"
+    case A11 = "Apple A11 Bionic"
+    case A12 = "Apple A12 Bionic"
+    case A12X = "Apple A12X Bionic"
+    case A12Z = "Apple A12Z Bionic"
+    case A13 = "Apple A13 Bionic"
+    case A14 = "Apple A14 Bionic"
+    case A15 = "Apple A15 Bionic"
+    
+    // M Seris
+    case M1 = "Apple M1"
+    case M1Pro = "Apple M1 Pro"
+    case M1Max = "Apple M1 Max"
+}
